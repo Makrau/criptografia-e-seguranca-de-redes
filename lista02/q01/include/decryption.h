c5de6d770fd973a5e7c2894380c5c664c4920738
Wilton Rodrigues <wiltonsr94@gmail.com>
2017-05-22 10:47:44 -0300

    Adding libs to decrypt and encrypt functions

---
+++ lista02/q01/include/decryption.h
@@ -0,0 +1,6 @@
+#ifndef DECRYPTION_H
+#define DECRYPTION_H
+
+#include "file_manipulate.h"
+
+#endif
---
+++ lista02/q01/include/encryption.h
@@ -0,0 +1,6 @@
+#ifndef ENCRYPTION_H
+#define ENCRYPTION_H
+
+#include "file_manipulate.h"
+
+#endif
