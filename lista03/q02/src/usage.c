219d4f40b1e58a4ff2ff1c0abe6cf6a2924a48bc
Wilton Rodrigues <wiltonsr94@gmail.com>
2017-05-27 21:42:53 -0300

    Adding usage

---
+++ lista03/q02/src/usage.c
@@ -0,0 +1,23 @@
+#include "libusage.h"
+
+void print_usage (FILE* stream, int exit_code, const char* program_name){
+  fprintf (stream, "\nUsage:  %s [options]... param...\n", program_name);
+  fprintf (stream,
+           "  {-h  --help}                       Display this usage information.\n"
+           "  {-d  --determine} [param]          Determine if a param is prime.\n"
+           "                                                                    \n");
+  exit (exit_code);
+}
+
+
+/* A string listing valid short options letters. */
+const char* const short_options = "hd:";
+
+/* An array describing valid long options. */
+const struct option long_options[] = {
+  { "help",       no_argument,        NULL, 'h' },
+  { "determine",  required_argument,  NULL, 'd' },
+  { NULL,         no_argument,        NULL, 0   }   /* Required at end of array. */
+};
+
+int next_option;
